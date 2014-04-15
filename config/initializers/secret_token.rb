5d8a7bfcad808e18def33c673b1195c924364ce2
Richard Yang <elyrly@gmail.com>
2014-04-15 12:29:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Glucose::Application.config.secret_key_base = 'cf4a59c32dffe92976efa1f4cc384d3ce540e9dd842d42c8521ba408624c95750fff16af08887ba3314b6dee743e5a7604e07f2b5f07f773a7eea646c1da6118'
